36a03c86300796b53b8da4bb188700362e9c36bc
eliaswisa <elia030688@gmail.com>
2020-11-24 23:49:09 +0200

    start working on the code from here

--- src/WGraph_DS.java
+++ src/WGraph_DS.java
@@ -1,65 +1,318 @@
 package ex1.src;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
-public class WGraph_DS implements weighted_graph {
+public class WGraph_DS implements weighted_graph, Serializable {
+    private HashMap<Integer, node_info> graphNodes = new HashMap<Integer, node_info>();
+    private int MC, edgesCounter, verticesCount;
+    private HashMap<Integer, HashMap<Integer, Double>> edges = new HashMap<Integer, HashMap<Integer, Double>>();
+    private static int nodesCounterDefault = 0;
+    private Collection<node_info> nada = new ArrayList<node_info>();
+
+    /***default consructor
+     *
+     *
+     *
+     */
+    public WGraph_DS() {
+
+
+    }
+
+    /**
+     * copy constructor
+     *
+     * @param g
+     */
+    public WGraph_DS(weighted_graph g) {
+        // TODO: 23/11/2020 check the copy constructor
+        if (g != null && g.getV() != null) {
+            graphNodes = new HashMap<>();
+            for (node_info nodes : g.getV()) {
+                graphNodes.put(nodes.getKey(), new NodeData(nodes));
+            }//copy every vertex from vertices list of g
+            edgesCounter = g.edgeSize();
+        }
+    }
+
+    /***
+     * checking if there is an edge between two nodes
+     * @param node1
+     * @param node2
+     * @return
+     */
+    @Override
+    public boolean hasEdge(int node1, int node2) {
+        boolean flag = false;
+        if (this.graphNodes.get(node1) == null && this.graphNodes.get(node2) == null) {
+            return false;
+        }
+        if (this.edges != null) {
+            if (this.edges.get(node1)!=null && this.edges.get(node2)!=null) {
+                if (this.edges.get(node1).get(node2) != null && this.edges.get(node2).get(node1) != null)//checks if &only if
+                     {
+                    flag = true;
+                }
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * gets node from the Wgraph map by inputing key
+     *
+     * @param key - the node_id
+     * @return node_info
+     */
     @Override
     public node_info getNode(int key) {
+        if (graphNodes != null) {
+            if (graphNodes.containsKey(key)) return graphNodes.get(key);
+        }
         return null;
     }
 
-    @Override
-    public boolean hasEdge(int node1, int node2) {
-        return false;
-    }
-
+    /**
+     * getting the edge weight between two nodes
+     *
+     * @param node1
+     * @param node2
+     * @return double edge weight
+     */
     @Override
     public double getEdge(int node1, int node2) {
-        return 0;
+        if (!hasEdge(node1, node2)) {
+            return -1;
+        }
+        return this.edges.get(node1).get(node2);
     }
 
+    /**
+     * adding node to the Wgraph hashMap by her key
+     *
+     * @param key
+     */
     @Override
+
     public void addNode(int key) {
+        if (this.graphNodes != null) {
+            if (this.graphNodes.get(key) == null) {
+                graphNodes.put(key, new NodeData(key));
+                this.MC++;
+                this.verticesCount++;
 
+            }
+        } else {
+            graphNodes.put(key, new NodeData(key));
+            this.MC++;
+            this.verticesCount++;
+        }
     }
 
+    /**
+     * connect between two nodes an edge who got w as weight
+     *
+     * @param node1
+     * @param node2
+     * @param w
+     */
     @Override
-    public void connect(int node1, int node2, double w) {
 
+    public void connect(int node1, int node2, double w) {
+        boolean direction1 = false;
+        boolean direction2 = false;
+        boolean directioncheck1 = true;
+        boolean directioncheck2 = true;
+        if (w < 0) {
+            System.out.println("no distance between nodes");
+            return;
+        }
+        if (this.edges.get(node1) != null) {
+            if (this.edges.get(node1).containsKey(node2)) {
+                directioncheck1 = false;
+            }
+            this.edges.get(node1).put(node2, w);
+            direction1 = true && directioncheck1;
+
+        } else {
+            this.edges.put(node1, new HashMap<Integer, Double>());
+            this.edges.get(node1).put(node2, w);
+            direction1 = true;
+        }
+
+        if (this.edges.get(node2) != null) {
+            if (this.edges.get(node2).containsKey(node1)) {
+                directioncheck2 = false;
+            }
+            this.edges.get(node2).put(node1, w);
+            direction2 = true && directioncheck2;
+
+
+        } else {
+            this.edges.put(node2, new HashMap<Integer, Double>());
+            this.edges.get(node2).put(node1, w);
+            direction2 = true;
+        }
+        if (direction1 && direction2) {
+            edgesCounter++;
+        }
     }
 
     @Override
     public Collection<node_info> getV() {
-        return null;
+        if (this.graphNodes != null) {
+            return this.graphNodes.values();
+        } else return nada;
     }
 
     @Override
     public Collection<node_info> getV(int node_id) {
-        return null;
+        Collection<node_info> niCollection = new ArrayList<>();
+
+        if (this.graphNodes.get(node_id) == null) {
+            return niCollection;
+        }
+        //use the edges for the list of the neighbours
+        if (!this.edges.isEmpty()) {
+            Collection<Integer> nodeNeighbours = this.edges.get(node_id).keySet();
+
+            for (int iter : nodeNeighbours) {
+                niCollection.add(this.graphNodes.get(iter));
+            }
+        }
+        return niCollection;
     }
 
     @Override
     public node_info removeNode(int key) {
-        return null;
+        node_info dynamicNode = this.graphNodes.get(key);
+        if (this.graphNodes.get(key) == null) {
+            return dynamicNode;
+        }
+        Collection<node_info> nodeNi = this.getV(key);
+        for (node_info iterNode : nodeNi) {
+            this.removeEdge(key, iterNode.getKey());
+            edgesCounter--;
+            //this.graphNodes.remove(key);
+        }
+        this.graphNodes.remove(key);
+        verticesCount--;
+        MC++;
+        return dynamicNode;
     }
 
     @Override
     public void removeEdge(int node1, int node2) {
-
+        if (this.hasEdge(node1, node2)) {
+            this.edges.get(this.graphNodes.get(node1).getKey()).remove(node2);
+            this.edges.get(this.graphNodes.get(node2).getKey()).remove(node1);
+        }
     }
 
     @Override
     public int nodeSize() {
-        return 0;
+        return this.graphNodes.values().size();
     }
 
     @Override
     public int edgeSize() {
-        return 0;
+        return this.edgesCounter;
     }
 
     @Override
     public int getMC() {
-        return 0;
+        return this.MC;
+    }
+
+    /////////////////////////inner class of node info
+    protected class NodeData implements node_info {
+        ///////fill  the fields
+        private int key;
+        private String info;
+        private double tag;
+
+
+        public double getWeight(int kNode) {
+            return 0;
+        }
+
+//        protected boolean isVisited() {
+//            return visited;
+//        }
+//
+//        protected void setVisited(boolean b) {
+//            this.visited = b;
+//        }
+
+        /**
+         * constructor by key
+         *
+         * @param key
+         */
+        public NodeData(int key) {
+            this.key = key;
+            this.tag = -1.0;
+            this.info = "";
+
+        }
+
+        public NodeData() {
+            this.key = nodesCounterDefault;
+            nodesCounterDefault++;
+            this.tag = -1.0;
+            this.info = "";
+        }
+
+        /**
+         * copy constructor
+         *
+         * @param n
+         */
+        // TODO: 24/11/2020 write node_info copy constructor
+        public NodeData(node_info n) {
+
+
+            //            this.key = n.getKey();
+//            this.info = n.getInfo();
+//            allNodeNi = new HashMap<>();
+//            for (int i : ((NodeInfo) n).allNodeNi.keySet()) {
+//                allNodeNi.put(i, ((NodeInfo) n).allNodeNi.get(i));
+//            }
+        }
+
+        public boolean hasNi(int key)// check if there's node between this node and a given node(by key)
+        {
+            return graphNodes.containsKey(key);
+        }
+
+        @Override
+        public int getKey() {
+            return this.key;
+        }
+
+        @Override
+        public String getInfo() {
+            return this.info;
+        }
+
+        @Override
+        public void setInfo(String s) {
+            this.info = s;
+        }
+
+        @Override
+        public double getTag() {
+            return tag;
+        }
+
+        @Override
+        public void setTag(double t) {
+            this.tag = t;
+        }
+
+
     }
 }
